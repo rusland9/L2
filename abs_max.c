68888b0cd95dd1ce5775c593e5e5a83370573d4b
rusland9 <31597491+rusland9@users.noreply.github.com>
2017-12-14 10:53:05 +0300

    Update abs_max.c

--- abs_max.c
+++ abs_max.c
@@ -4,11 +4,11 @@
 
 int abs_max(){
     int j;
-    max=mas[0];
+    max1=mas[0];
     for (j=0; j<(i-1); j++)
         if ( abs(mas[j])>abs(mas[j+1]) ){
-            max=mas[j+1];
+            max1=mas[j+1];
             maxi=j+1
         }
-    return max;
+    return max1;
 }  
