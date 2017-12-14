d8bbd0032fb2efae31fcbb92f43bab6afa55ae73
rusland9 <31597491+rusland9@users.noreply.github.com>
2017-12-14 10:39:26 +0300

    Update abs_max.c

--- abs_max.c
+++ abs_max.c
@@ -3,13 +3,12 @@
 #include "abs_max.h"
 
 int abs_max(){
-    int j, n;
+    int j;
+    max=mas[0];
     for (j=0; j<(i-1); j++)
         if ( abs(mas[j])>abs(mas[j+1]) ){
-            n=mas[j];
-            mas[j]=mas[j+1]
-            mas[j+1]=n;
+            max=mas[j+1];
+            maxi=j+1
         }
-    max=mas[i]
     return max;
 }  
