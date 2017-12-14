a11c5f63470ebe39508314815ef07157c0e4677f
rusland9 <31597491+rusland9@users.noreply.github.com>
2017-12-14 04:39:04 +0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "get_name.h"
-#include "print_str.h"
+#include "abs_max.h"
+#include "abs_min.h"
+#include "diff.h"
+#include "sum.h"
+
+#define N   100
 
 int main(){
-    char hello[90] = "Hello, ";
-    char* result;
-    result = get_name();
-    print_str(strncat(hello, result, 80));
-    free(result);
+    char mas[N];
+    int a, i;
+    scanf("%d", &a);
+    for (i=0; i<N; i++)
+        scanf("%d", &mas[i]);
+    switch(a){
+        case 0:
+            abs_max(mas[]);
+            break;
+        case 1:
+            abs_min(mas[]);
+            break;
+        case 2:
+            diff(mas[]);
+            break;
+        case 3:
+            sum(mas[]);
+            break;
+        default:
+            printf("Данные некорректны");
+            break;
+    }
     return 0;
 }
